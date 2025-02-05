3518f1f0ee4c33620532c2ffea3be865581b273b
Ahmed-arrayes <abdelhamedahmed45499@gmail.com>
2025-02-05 23:37:54 +0200

    Add user registration and login forms

--- forms.py
+++ forms.py
@@ -12,3 +12,21 @@ class QuizSelectionForm(FlaskForm):
     count = IntegerField('عدد الأسئلة', validators=[DataRequired(), NumberRange(min=1, max=20)], default=10)
     difficulty = SelectField('مستوى الصعوبة', choices=[('سهل', 'سهل'), ('متوسط', 'متوسط'), ('صعب', 'صعب')], validators=[DataRequired()])
     submit = SubmitField('ابدأ الاختبار')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('اسم المستخدم', validators=[DataRequired(), Length(min=3, max=25)])
+    email = StringField('البريد الإلكتروني', validators=[DataRequired(), Email()])
+    password = PasswordField('كلمة المرور', validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField('تأكيد كلمة المرور', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('تسجيل')
+
+class LoginForm(FlaskForm):
+    email = StringField('البريد الإلكتروني', validators=[DataRequired(), Email()])
+    password = PasswordField('كلمة المرور', validators=[DataRequired()])
+    remember_me = SelectField('تذكرني', choices=[('yes', 'نعم'), ('no', 'لا')])
+    submit = SubmitField('تسجيل الدخول')
+
+class UpdateProfileForm(FlaskForm):
+    username = StringField('اسم المستخدم', validators=[DataRequired(), Length(min=3, max=25)])
+    email = StringField('البريد الإلكتروني', validators=[DataRequired(), Email()])
+    submit = SubmitField('تحديث')
